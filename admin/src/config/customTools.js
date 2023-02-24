b4d161e0c37881255177d8213b9c2bc695b6d1b1
Eric Chautems <eric.chautems@kooko.ch>
2023-02-24 11:30:48 +0100

    chore: initial config for kooko strapi plugin

--- admin/src/config/customTools.js
+++ admin/src/config/customTools.js
@@ -13,6 +13,7 @@ import Marker from '@editorjs/marker'
 import CheckList from '@editorjs/checklist'
 import Delimiter from '@editorjs/delimiter'
 import InlineCode from '@editorjs/inline-code'
+import ColorPlugin from "editorjs-text-color-plugin"
 
 const customTools = {
   embed: Embed,
@@ -62,6 +63,21 @@ const customTools = {
   checklist: {
     class: CheckList,
     inlineToolbar: true,
+  },
+  Color: {
+    class: ColorPlugin, // if load from CDN, please try: window.ColorPlugin
+    config: {
+      colorCollections: ['#009f79','#e0e7e5','#809d94','#003b29','#00190f', '#32324d', '#ffffff', '#000000'],
+      defaultColor: '#32324d',
+      type: 'text',
+    }
+  },
+  Marker: {
+    class: ColorPlugin, // if load from CDN, please try: window.ColorPlugin
+    config: {
+      defaultColor: '#FFBF00',
+      type: 'marker',
+    }
   },
   delimiter: Delimiter,
   inlineCode: InlineCode,
